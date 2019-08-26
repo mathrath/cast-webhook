f84a964bfdd53a439c746a7c632c7c201156a809
Matt Rathbun <matt@rathbun.cc>
2019-08-25 21:53:56 -0400

    Add ability to configure by environment variable

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ const Client = require('./src/client');
 
 // Parse command line arguments with yargs
 const argv = yargs
-  .option('host', {
+  .env('CAST_WEBHOOK')  // Prefix for configuring by environment variable
+  .option('host', {  // CAST_WEBHOOK_HOST
     alias: 'h',
     describe: 'cast device to watch',
     type: 'string',
     demandOption: true,
   })
-  .option('onStart', {
+  .option('start', {  // CAST_WEBHOOK_START
     alias: 's',
     describe: 'webhook to call when a session starts',
     type: 'string',
   })
-  .option('onEnd', {
+  .option('end', {  // CAST_WEBHOOK_END
     alias: 'e',
     describe: 'webhook to call when a session ends',
     type: 'string',
@@ -24,7 +25,7 @@ const argv = yargs
 // Here we go!
 const client = new Client({
   host: argv.host,
-  onStart: argv.onStart,
-  onEnd: argv.onEnd,
+  onStart: argv.start,
+  onEnd: argv.end,
 });
 client.start();
